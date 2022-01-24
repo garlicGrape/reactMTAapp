e3f32165398892379e642310febe19e3e50a9c30
SanchitKumar16 <60677508+SanchitKumar16@users.noreply.github.com>
2022-01-23 21:32:46 -0500

    work for 1/23, morebackend work and python mta server

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const axios = require("axios");
 const app = express(); //Instaniate an Express object (where the app gets created)
 const fs = require("fs"); //Loads the file system 
 
+const API_DOMAIN = "http://localhost:5000";
+
 require("dotenv").config({silent: true});
 const mongoose = require("mongoose");
 const db_url = process.env.MONGO_DB_URL;
@@ -22,6 +24,39 @@ app.use((req, res, next) => {
     );
     next();
 });
+
+// parse stations json file
+const stationsData = JSON.parse(
+    fs.readFileSync("../mtaserver/data/stations.json").toString()
+);
+const stationIDs = Object.keys(stationsData);
+
+
+// returns array of station objects, with id, name, and list of routes
+app.get("/allStations", (req, res, next) => {
+    const ids = stationIDs.join(",");
+    const endpoint = API_DOMAIN + "/by-id/" + ids;
+    axios
+      .get(endpoint)
+      .then((response) => {
+        const data = response.data.data
+          .filter((station) => station.routes.length > 0) // remove stations with no routes currently available
+          .map((station) => {
+            return {
+              id: station.id,
+              name: station.name,
+              routes: station.routes.sort(),
+            };
+          });
+        console.log(data);
+        res.send(data);
+      })
+      .catch((error) => {
+        console.log(error);
+        next(error);
+      });
+  });
+  
 
 
 
